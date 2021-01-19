01d93ae5c949c40a1fdc83cbeb750f1b137bdd79
pangshuhai <pangshuhai@qdspr.com>
2021-01-19 13:08:12 +0800

    fix

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<main id="main" role="main" class="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+    <main id="main" role="main" class="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
         <h3 class="archive-title"><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
@@ -10,15 +10,19 @@
         ), '', ''); ?></h3>
         <?php if ($this->have()): ?>
     	<?php while($this->next()): ?>
-            <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-    			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+            <article class="box shadow-sm border rounded bg-white mb-3" itemscope itemtype="http://schema.org/BlogPosting">
+                <div class="box-title border-bottom p-3">
+                    <h1 itemprop="name headline" class="post-title">
+                        <a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                    </h1>
+                </div>
     			<ul class="post-meta">
     				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
     				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
     				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
                     <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
     			</ul>
-                <div class="post-content" itemprop="articleBody">
+                <div class="box-body p-3" itemprop="articleBody">
                     <?php $this->excerpt(350, ''); ?>
                     <p><a href="<?php $this->permalink() ?>">...阅读剩余</a></p>
                     <p> text archive.php</p>
--- index.php
+++ index.php
@@ -16,13 +16,28 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 	<?php while($this->next()): ?>
 		<div class="box shadow-sm border rounded bg-white mb-3" itemscope itemtype="http://schema.org/BlogPosting">
-			<h6 itemprop="name headline" class="font-weight-bold text-dark mb-0"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h6>
-			<ul class="post-meta">
-				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-			</ul>
+			<div class="box-title border-bottom p-3">
+				<h1 itemprop="name headline" class="post-title">
+					<a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+				</h1>
+			</div>
+			<div class="post-state">
+				<div class="post-state-btns" itemprop="author" itemscope itemtype="http://schema.org/Person">
+					<?php _e('作者: '); ?><span><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></span>
+				</div>
+				<div class="post-state-btns">
+					<?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+					<i class="feather-rewind"></i>18 <span>Coments</span>
+				</div>
+				<div class="post-state-btns">
+					<?php _e('分类: '); ?><?php $this->category(','); ?>
+					<i class="feather-link"></i>193 <span>Shared </span>
+				</div>
+				<div class="post-state-btns" temprop="interactionCount">
+					<a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a>
+					<i class="feather-hard-drive"></i>13 <span>Saved </span>
+				</div>
+			</div>
 			<div class="box-body p-3" itemprop="articleBody">
 				<?php $this->excerpt(350, ''); ?>
 				<p><a href="<?php $this->permalink() ?>">...阅读剩余部分</a></p>
