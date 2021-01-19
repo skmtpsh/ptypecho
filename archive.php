129d0f886841e48636993a83df1289d730dedf4a
pangshuhai <pangshuhai@qdspr.com>
2021-01-19 09:08:27 +0800

    yuedu

--- archive.php
+++ archive.php
@@ -19,7 +19,7 @@
                     <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
     			</ul>
                 <div class="post-content" itemprop="articleBody">
-        			<?php $this->excerpt(350, '- 阅读剩余部分 -'); ?>
+        			<?php $this->excerpt(350, '...' + <a href="<?php $this->permalink() ?>" + '阅读剩余部分' + </a>); ?>
                 </div>
     		</article>
     	<?php endwhile; ?>
