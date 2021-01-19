2b2c33ce9ab6ba522b3976b568d1703334a24697
pangshuhai <pangshuhai@qdspr.com>
2021-01-19 11:25:15 +0800

    logo

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 <![endif]-->
 <nav id="Header" class="navbar navbar-expand p-0 osahan-nav-top">
     <div class="container">
-      <a href="<?php $this->options->siteUrl(); ?>" class="navbar-brand mr-2"><img src="<?php $this->options->themeUrl('img/logo6.png'); ?>" width="60" /></a>
+      <a href="<?php $this->options->siteUrl(); ?>" class="navbar-brand mr-2"><img src="<?php $this->options->themeUrl('img/logo.png'); ?>" width="60" /></a>
       <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search" class="d-none d-sm-inline-block form-inline mr-auto my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
           <label for="s" class="sr-only"><?php _e('输入关键字搜索'); ?></label>
