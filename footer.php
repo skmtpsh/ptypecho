e9e4992888c44407877b3e9729c7488fe99e0f6e
pangshuhai <pangshuhai@qdspr.com>
2021-01-22 10:26:06 +0800

    fix beian

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <!-- <?php _e('柚米版权所有 '); ?>.  -->
-    <a hre="https://beian.miit.gov.cn/">鲁ICP备2021002545号</a>
+    <a href="https://beian.miit.gov.cn/">鲁ICP备2021002545号</a>
 </footer><!-- end #footer -->
 <?php $this->footer(); ?>
 </body>
