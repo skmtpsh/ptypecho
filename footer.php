fff2481f7954aa5aed38bc47821dd402b777eb12
pangshuhai <pangshuhai@qdspr.com>
2021-01-25 08:27:27 +0800

    fff

--- footer.php
+++ footer.php
@@ -8,6 +8,15 @@
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <!-- <?php _e('柚米版权所有 '); ?>.  -->
     <a href="https://beian.miit.gov.cn/">鲁ICP备2021002545号</a>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7d100c0973614817f55306a81da71486";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 </footer><!-- end #footer -->
 <?php $this->footer(); ?>
 </body>
