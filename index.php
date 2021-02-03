d8bf2ba4161b31e7d26b139a459e86f6fc8a2cd3
pangshuhai <pangshuhai@qdspr.com>
2021-02-03 15:18:45 +0800

    feat: 样式

--- index.php
+++ index.php
@@ -14,19 +14,15 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 <div class="col-mb-12 col-8" id="main" role="main">
 	<?php while($this->next()): ?>
-        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h2>
-			<ul class="post-meta">
-				<li itemprop="author" itemscope itemtype="http://schema.org/Person">
-					<?php _e('作者: '); ?>
-					<?php $this->author->gravatar(); ?>
-					<a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a>
-				</li>
-				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-				<li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
-				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-			</ul>
+				<article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+						<div class="post-head" itemprop="author" itemscope itemtype="http://schema.org/Person">
+							<div class="post-head-img"><?php $this->author->gravatar(); ?></div>
+							<div class="post-head-tit">
+								<p><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></p>
+								<time class="time" datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+							</div>
+						</div>
+						<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h2>
             <div class="post-content" itemprop="articleBody">
 							<!-- <?php $this->content('- 阅读剩余部分 -'); ?> -->
 							<?php
@@ -38,7 +34,12 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 									echo '</br><p class="more"><a href="',$this->permalink(),'" title="',$this->title(),'">阅读剩余部分...</a></p>';
 								}
 							?>
-            </div>
+						</div>
+						<ul class="post-meta">
+							<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+							<li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
+							<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+						</ul>
         </article>
 	<?php endwhile; ?>
 
