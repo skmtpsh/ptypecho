0b1fa9275b80bd89ec5947f848355e438359a150
pangshuhai <pangshuhai@qdspr.com>
2021-02-03 09:16:04 +0800

    feat: 修改评论头像

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
-    <p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. 备案/许可证编号: 鲁ICP备2021002545</p>
+    <p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. 备案/许可证编号: <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2021002545</a></p>
     <p><?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.</p>
 </footer><!-- end #footer -->
 
