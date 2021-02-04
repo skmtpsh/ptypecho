0e86908f4f332ae3fdebf9f8307e243b3a349a30
pangshuhai <pangshuhai@qdspr.com>
2021-02-04 11:43:21 +0800

    feat: 样式

--- archive.php
+++ archive.php
@@ -30,14 +30,16 @@
         <?php if ($this->have()): ?>
     	<?php while($this->next()): ?>
             <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-    			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h2>
-    			<ul class="post-meta">
-    				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-    				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-                    <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-				    <li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
-                    <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-    			</ul>
+                <div class="post-head" itemprop="author" itemscope itemtype="http://schema.org/Person">
+                    <div class="post-head-img"><?php $this->author->gravatar(); ?></div>
+                    <div class="post-head-tit">
+                        <p><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></p>
+                        <time class="time" datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+                    </div>
+                </div>
+                <h2 class="post-title" itemprop="name headline">
+                    <span class="tags"><?php $this->category(','); ?></span><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                </h2>
                 <div class="post-content" itemprop="articleBody">
                 <?php
                     if (preg_match('/<!--more-->/',$this->content)||mb_strlen($this->content, 'utf-8') < 270) {
@@ -49,6 +51,10 @@
                     }
                 ?>
                 </div>
+                <ul class="post-meta">
+                    <li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
+                    <li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+                </ul>
     		</article>
     	<?php endwhile; ?>
         <?php else: ?>
--- post.php
+++ post.php
@@ -4,17 +4,23 @@
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->sticky(); $this->title() ?></a></h1>
-        <ul class="post-meta">
-            <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-            <li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-            <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-			<li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
-
-        </ul>
+        <div class="post-shead" itemprop="author" itemscope itemtype="http://schema.org/Person">
+            <span>
+                <?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
+            </span>
+            <a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author" class="author">
+                <?php $this->author->gravatar(); ?>
+                <?php $this->author(); ?>
+            </a>
+        </div>
         <div class="post-content" itemprop="articleBody">
             <?php $this->content(); ?>
         </div>
         <p itemprop="keywords" class="tagsbox"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></p>
+        <ul class="post-meta">
+            <li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+			<li><?php _e('阅读:'); ?>(<?php $this->viewsCount(); ?>)</li>
+        </ul>
     </article>
     <article class="post-near pd-20">
         <p>上一篇：<?php $this->thePrev('%s','没有了'); ?></p>
