cea8cab2e958ec37184f69cca7b849bcfa1a4df7
pangshuhai <pangshuhai@qdspr.com>
2021-02-05 17:06:16 +0800

    ico

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@ var app = new Vue({
             visible: false,
             images: [
                 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20170723%2F504dea5ff5404c9c9d18d4e1426c74b9_th.jpg&refer=http%3A%2F%2Fimg.mp.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615107736&t=ad95d4d6cef33bc3420f41150f51df63',
-                'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.huodongjia.com%2Fevent%2F2015-12-01%2Fevent155380.jpg&refer=http%3A%2F%2Fpic.huodongjia.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615107736&t=c7c22e8b9590e88dc76495fed13c02d2'
+                'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.huodongjia.com%2Fevent%2F2015-12-01%2Fevent155380.jpg&refer=http%3A%2F%2Fpic.huodongjia.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615107736&t=c7c22e8b9590e88dc76495fed13c02d2',
                 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1545262284,771499180&fm=26&gp=0.jpg',
                 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fi3.sinaimg.cn%2FIT%2Fcr%2F2012%2F0129%2F1137216345.jpg&refer=http%3A%2F%2Fi3.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615107595&t=4f189e4a317734476d6b32a8c0da5d71'
             ]
