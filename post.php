bf3f16601e51ddabced9dcd6ca3dcbcab41ccbed
pangshuhai <pangshuhai@qdspr.com>
2021-02-07 15:34:49 +0800

    ico

--- post.php
+++ post.php
@@ -21,7 +21,7 @@
         </ul>
     </article>
     <div class="Copyrightnew">文章由官网发布，如若转载，请注明出处：<?php $this->permalink() ?></div>
-    <div class="widget-column"><span class="widget-column__head">·&nbsp; 已收录至专栏 &nbsp;·</span></div>
+    <!-- <div class="widget-column"><span class="widget-column__head">·&nbsp; 已收录至专栏 &nbsp;·</span></div> -->
     <article class="post-near pd-20">
         <p>上一篇：<?php $this->thePrev('%s','没有了'); ?></p>
         <p>下一篇：<?php $this->theNext('%s','没有了'); ?></p>
