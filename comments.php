1ca0b68571d99782823e04eb90a4c124d98d0212
pangshuhai <pangshuhai@qdspr.com>
2021-02-07 16:06:15 +0800

    ico

--- comments.php
+++ comments.php
@@ -71,28 +71,32 @@
         </div>
     	<h4 id="response"><?php _e('发表评论'); ?></h4>
     	<form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form" class="comment-form">
-            <?php if($this->user->hasLogin()): ?>
-    		<p><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
-            <?php else: ?>
-    		<p>
-    			<input type="text" name="author" id="author" class="text" placeholder="请输入称呼" value="<?php $this->remember('author'); ?>" required />
-    		</p>
-    		<p>
-    			<input type="email" name="mail" id="mail" class="text" placeholder="请输入QQ邮箱" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
-    		</p>
-    		<p>
-    			<input type="url" name="url" id="url" class="text" placeholder="请输入网址 <?php _e('http://'); ?>" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
-            </p>
-            <p>
-                <?php spam_protection_math();?>
-            </p>
-            <?php endif; ?>
-    		<p>
-                <textarea rows="8" name="text" id="textarea" class="textarea" maxlength="200" placeholder="请输入5-200以内的文字" required style="min-height: 100px;"><?php $this->remember('text'); ?></textarea>
-            </p>
-    		<p>
-                <button type="submit" class="submit primary-btn"><?php _e('提交评论'); ?></button>
-            </p>
+
+
+            <div class="row">
+                <?php if($this->user->hasLogin()): ?>
+                <p><a href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
+                <?php else: ?>
+                <div class="col-mb-12 col-4">
+                    <input type="text" name="author" id="author" class="text" placeholder="请输入称呼" value="<?php $this->remember('author'); ?>" required />
+                </div>
+                <div class="col-mb-12 col-4">
+    			    <input type="email" name="mail" id="mail" class="text" placeholder="请输入QQ邮箱" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
+                </div>
+                <div class="col-mb-12 col-4">
+    			    <input type="url" name="url" id="url" class="text" placeholder="请输入网址 <?php _e('http://'); ?>" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
+                </div>
+                <div class="col-mb-12 col-12">
+                    <?php spam_protection_math();?>
+                </div>
+                <?php endif; ?>
+                <div class="col-mb-12 col-12">
+                    <textarea rows="8" name="text" id="textarea" class="textarea" maxlength="200" placeholder="请输入5-200以内的文字" required style="min-height: 100px;"><?php $this->remember('text'); ?></textarea>
+                </div>
+                <div class="col-mb-12 col-12">
+                    <button type="submit" class="submit primary-btn"><?php _e('提交评论'); ?></button>
+                </div>
+            </div>
     	</form>
     </div>
     <?php else: ?>
