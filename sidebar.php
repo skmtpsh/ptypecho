a21c5a95e881370976e3c43776026e4764ccf4f6
pangshuhai <pangshuhai@qdspr.com>
2021-02-24 16:36:05 +0800

    fix: modify

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,13 @@
 <div class="col-mb-12 col-3 kit-hidden-tb" id="secondary" role="complementary">
     <el-card shadow="never" style="border: 0 none;" :body-style="{ padding: '0px' }">
         <ul class="modle-list">
-            <li><i class="el-icon-collection"></i> 小学古诗词 </li>
+            <li>
+                <a href="#" rel="noreferrer noopener" target="_blank">
+                    <i class="el-icon-collection"></i>
+                    <strong>小学古诗词</strong>
+                    <span>万2.5低佣<i class="el-icon-arrow-right"></i></span>
+                </a>
+            </li>
         </ul>
     </el-card>
     <section class="widget">
