77655968744b535116a2b3591f0bbccd11df3eb4
pangshuhai <pangshuhai@qdspr.com>
2021-02-24 16:28:25 +0800

    fix: modify

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <div class="col-mb-12 col-3 kit-hidden-tb" id="secondary" role="complementary">
-    <el-card shadow="never">
+    <el-card shadow="never" style="border: 0 none;" :body-style="{ padding: '0px' }">
         <ul class="modle-list">
             <li><i class="el-icon-collection"></i> 小学古诗词 </li>
         </ul>
