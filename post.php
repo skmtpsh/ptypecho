7209c97805a2cc1c46ab043b956f93e3c191c0c9
skmtpsh <pangshuhai.com>
2021-02-28 11:24:23 +0800

    fix

--- post.php
+++ post.php
@@ -18,9 +18,6 @@
         <p itemprop="keywords" class="tagsbox"><?php $this->tags(' ', true, 'none'); ?></p>
         <ul class="post-meta">
             <li class="kit-show">
-                <i class="el-icon-chat-line-square"></i> 转发
-            </li>
-            <li class="kit-show">
                 <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><i class="el-icon-date"></i> 发布于(<?php $this->date(); ?>) </time>
             </li>
 			<li><i class="el-icon-view"></i> 阅读(<?php $this->viewsCount(); ?>)</li>
