901100d2086bc493179e8d421a5d43c6aaf130fe
pangshuhai <pangshuhai@qdspr.com>
2021-03-06 08:22:58 +0800

    fix

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
   :before-close="handleBaikeClose"
   size="80%"
 >
-    <el-collapse v-model="activeNames" @change="handleChange">
+    <el-collapse v-model="activeNames">
         <el-collapse-item title="明朝皇帝" name="1">
             <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
         </el-collapse-item>
